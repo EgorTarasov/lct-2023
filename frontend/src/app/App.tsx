2e58ee827ba82955d38078198ea8f41dc7e9b395
Kireev Kirill <kireev.kd@perfema.com>
2023-11-04 19:17:18 +0300

    fix tsconfig

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,23 +1,18 @@
 import "./index.css";
 import ViteSvg from "../vite.svg";
-import { useState } from "react";
-import { Button, DialogBase } from "@/ui";
-import { DesktopHeading } from "@/components/navigation/DesktopHeading";
-import { ID } from "@/constants/ids";
 import Router from "./router";
 import { BrowserRouter } from "react-router-dom";
+import { DesktopHeading } from "@/components/navigation";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <DesktopHeading />
-        <ViteSvg />
-        <main id="content" tabIndex={-1}>
-          <Router />
-        </main>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <DesktopHeading />
+      <ViteSvg />
+      <main id="content" tabIndex={-1}>
+        <Router />
+      </main>
+    </BrowserRouter>
   );
 }
 
