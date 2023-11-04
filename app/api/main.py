dd0e739e4c2c6af758b020f11dcf4cf6dcc47f27
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-11-04 13:54:54 +0300

    changed routes to match nginx

--- app/api/main.py
+++ app/api/main.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api import auth
 from app.api import role
 
-router = APIRouter(prefix="/api")
+router = APIRouter()
 router.include_router(auth.router)
 router.include_router(role.router)
