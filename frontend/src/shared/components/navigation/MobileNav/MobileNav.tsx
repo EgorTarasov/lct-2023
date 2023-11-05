69150090a5f1e1f13c8764f9e67bf8828dbe1fd1
Aleksey Nizhgorodov <nizhgorodov.ao@perfema.com>
2023-11-06 00:00:08 +0200

    menu enhancement

--- frontend/src/shared/components/navigation/MobileNav/MobileNav.tsx
+++ frontend/src/shared/components/navigation/MobileNav/MobileNav.tsx
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NavIcon from "./assets/nav.svg";
-import { NavLink } from "react-router-dom";
-import { useEscape } from "@/hooks/useEscape";
-import { routes } from "../../../../app/routes";
+import { NavLink, useLocation } from "react-router-dom";
 import { Dialog } from "@headlessui/react";
+import { routes } from "../../../../app/routes.ts";
+import LightningIcon from "@/assets/lightning.svg";
+import UserIcon from "@/assets/user.svg";
+import ChevronIcon from "@/assets/chevron2.svg";
+import { twMerge } from "tailwind-merge";
+import CloseIcon from "@/assets/clear.svg";
+import { ThemeService } from "@/stores/theme.service.ts";
 
 export const MobileNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
   const currentRoute = routes.find((route) => route.path === location.pathname);
 
   const toggleMenu = () => {
@@ -14,15 +20,15 @@ export const MobileNav = () => {
   };
 
   return (
-    <nav className="sticky block sm:hidden top-0 z-10 w-full bg-nav-background">
+    <nav className="sticky block sm:hidden top-0 z-10 w-full bg-nav-background flex justify-between items-center px-4 py-2 shadow">
       <div className="flex items-center">
         <button
           id="nav-button"
           aria-expanded={isMenuOpen}
           aria-controls="mobile-menu"
           onClick={toggleMenu}
-          className="p-2">
-          <span className="sr-only">{isMenuOpen ? "Закрыть меню" : "Открыть меню"}</span>
+          className="p-1">
+          <span className="sr-only">{"Открыть меню"}</span>
           <NavIcon
             aria-hidden="true"
             focusable="false"
@@ -31,23 +37,57 @@ export const MobileNav = () => {
         </button>
         <h1 className="text-xl font-normal text-nav-text">{currentRoute?.title || ""}</h1>
       </div>
-      <Dialog open={isMenuOpen} onClose={toggleMenu}>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2" aria-label={"Ваша активность: 100%"}>
+          <LightningIcon className="w-6 h-6 text-primary" />
+          <span className="text-base text-text-primary">100</span>
+        </div>
+        <NavLink to={"/me"} aria-label="Перейти в профиль">
+          <UserIcon className="w-10 h-10 text-primary" />
+        </NavLink>
+      </div>
+      <Dialog open={isMenuOpen} onClose={toggleMenu} className="relative z-40">
         <Dialog.Panel>
           <Dialog.Title>Навигационное меню</Dialog.Title>
           <div id="mobile-menu" className="fixed inset-0 z-40 bg-nav-background">
-            <div className="flex justify-end p-4">
-              <button onClick={toggleMenu} className="p-2">
-                <span className="sr-only">Закрыть меню</span>
-                <NavIcon aria-hidden="true" focusable="false" className="w-10 h-10 text-primary" />
-              </button>
+            <div className="flex justify-between px-4 py-2 items-center">
+              <div className="flex items-center">
+                <button onClick={toggleMenu} className="p-1">
+                  <span className="sr-only">Закрыть меню</span>
+                  <CloseIcon
+                    aria-hidden="true"
+                    focusable="false"
+                    className="w-10 h-10 text-text-primary"
+                  />
+                </button>
+                <img src={ThemeService.themeConfig?.logoUrl} alt="Логотип" className="h-8" />
+              </div>
+              <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2" aria-label={"Ваша активность: 100%"}>
+                  <LightningIcon className="w-6 h-6 text-primary" />
+                  <span className="text-base text-text-primary">100</span>
+                </div>
+                <NavLink to={"/me"} aria-label="Перейти в профиль">
+                  <UserIcon className="w-10 h-10 text-primary" />
+                </NavLink>
+              </div>
             </div>
-            <ul className="p-4">
+            <ul className="px-4">
               {routes
-                .filter((p) => p.showInNav)
+                .filter((route) => route.showInNav)
                 .map((item) => (
-                  <li key={item.path} className="border-b border-nav-text">
-                    <NavLink to={item.path} onClick={toggleMenu} className="block py-2">
+                  <li
+                    key={item.path}
+                    className={twMerge(
+                      "border-b border-nav-text py-3",
+                      item.path === currentRoute?.path ? "text-primary" : ""
+                    )}>
+                    <NavLink
+                      to={item.path}
+                      onClick={toggleMenu}
+                      className="flex items-center justify-between">
                       {item.title}
+                      {item.path !== currentRoute?.path && <ChevronIcon className="w-6 h-6" />}
                     </NavLink>
                   </li>
                 ))}
