13890f6f684148053a285312a8bfa40fb074a065
Kireev Kirill <karjaakirill@gmail.com>
2023-11-05 14:16:46 +0300

    add transitions

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,44 @@
 import "./index.css";
-import Router from "./router";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { ThemeService } from "@/stores/theme.service.ts";
 import { observer } from "mobx-react-lite";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
+import "./transitions.scss";
+import PrivateRoute from "@/hoc/PrivateRoute";
+import { Login, MainPage, ResetPassword } from "../pages";
 
 const App = observer(() => {
+  const location = useLocation();
+
   if (!ThemeService.isLoaded)
     return <div className={"w-full h-full flex items-center justify-center"}>⏳Загрузка...</div>;
+  console.log("render", location.key);
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <SwitchTransition>
+      <CSSTransition key={location.key} classNames="fade" timeout={150} unmountOnExit>
+        <Routes location={location}>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <MainPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/tasks">
+            <Route index element={<h1>v</h1>} />
+            <Route path="program" element={<h1>d</h1>} />
+          </Route>
+          <Route path="/events" element={<MainPage />} />
+          <Route path="/me" element={<MainPage />} />
+          <Route path="/shop" element={<MainPage />} />
+          <Route path="/contacts" element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </CSSTransition>
+    </SwitchTransition>
   );
 });
 
