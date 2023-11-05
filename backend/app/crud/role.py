350deab8371555a856bf151ab030fe2e3a99ec5b
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-11-05 18:38:27 +0300

    minor refactoring for better type hinting

--- backend/app/crud/role.py
+++ backend/app/crud/role.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm import Session
 from app.models.role import SqlRole, RoleCreate, RoleDto
 
 
-async def create_role(db: Session, payload: RoleCreate) -> RoleDto:
+async def create_role(db: Session, payload: RoleCreate) -> SqlRole:
     role = SqlRole(**payload.model_dump())
     db.add(role)
     db.commit()
     db.refresh(role)
-    return RoleDto.model_validate(role)
+    return role
 
 
-async def get_roles(db: Session) -> list[RoleDto]:
-    return [RoleDto.model_validate(role) for role in db.query(SqlRole).all()]
+async def get_roles(db: Session) -> list[SqlRole]:
+    return db.query(SqlRole).all()
