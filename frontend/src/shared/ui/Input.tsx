4e9d297f36a84daf6b4f13b8fc68e46fed2abf47
Kireev Kirill <karjaakirill@gmail.com>
2023-11-05 13:01:25 +0300

    fix api

--- frontend/src/shared/ui/Input.tsx
+++ frontend/src/shared/ui/Input.tsx
@@ -1,6 +1,6 @@
 import { twMerge } from "tailwind-merge";
 // import ClearSvg from "@/assets/clear.svg"; WTF IS TRIGGERING TS ERROR
-import React from "react";
+import React, { useEffect } from "react";
 
 interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   appearance?: "primary";
@@ -9,6 +9,7 @@ interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "
   errorText?: string | null;
   allowClear?: boolean;
   icon?: JSX.Element;
+  onIconClick?: () => void;
   label?: string;
 }
 
@@ -19,45 +20,60 @@ export const Input: React.FC<InputProps> = ({
   error,
   errorText,
   icon,
+  onIconClick,
   allowClear,
   label,
   ...rest
-}) => (
-  <div className={twMerge("w-full flex flex-col", className)}>
-    {label && (
-      <label
-        className={twMerge("text-primary/60 mb-2", (error || errorText) && "text-error")}
-        htmlFor={rest.id ?? rest.name}>
-        {label}
-      </label>
-    )}
-    <div className="w-full group flex relative items-center">
-      <input
-        className={twMerge(
-          "w-full p-3",
-          "outline-none transition-colors border border-text-primary/20 group-hover:border-text-primary/60 focus:border-primary rounded-lg", // primary
-          (allowClear || icon) && "pr-10",
-          (error || errorText) && "border-error",
-          rest.disabled
-            ? "bg-text-primary/5 group-hover:bg-text-primary/5 text-text-primary/20"
-            : ""
+}) => {
+  useEffect(() => {
+    if (icon && !onIconClick) {
+      console.error(
+        "Input: icon is set but onIconClick is not. Use it for accessibility purposes!"
+      );
+    }
+  }, []);
+
+  return (
+    <div className={twMerge("w-full flex flex-col", className)}>
+      {label && (
+        <label
+          className={twMerge("text-primary/60 mb-2", (error || errorText) && "text-error")}
+          htmlFor={rest.id ?? rest.name}>
+          {label}
+        </label>
+      )}
+      <div className="w-full group flex relative items-center">
+        <input
+          className={twMerge(
+            "w-full p-3",
+            "outline-none transition-colors border border-text-primary/20 group-hover:border-text-primary/60 focus:border-primary rounded-lg", // primary
+            (allowClear || icon) && "pr-10",
+            (error || errorText) && "border-error",
+            rest.disabled
+              ? "bg-text-primary/5 group-hover:bg-text-primary/5 text-text-primary/20"
+              : ""
+          )}
+          onChange={(e) => onChange?.(e.target.value)}
+          {...rest}
+        />
+        {allowClear && rest.value && !rest.disabled && (
+          <button
+            type="button"
+            className="absolute w-5 right-3 text-text-primary/60 hover:text-text-primary"
+            onClick={() => onChange?.("")}>
+            x
+          </button>
         )}
-        onChange={(e) => onChange?.(e.target.value)}
-        {...rest}
-      />
-      {allowClear && rest.value && !rest.disabled && (
-        <button
-          className="absolute w-5 right-3 text-text-primary/60 hover:text-text-primary"
-          onClick={() => onChange?.("")}>
-          x
-        </button>
-      )}
-      {icon && !allowClear && (
-        <button className="absolute right-1 p-2 w-10 text-text-primary/60 hover:text-text-primary">
-          {icon}
-        </button>
-      )}
+        {icon && !allowClear && (
+          <button
+            type="button"
+            onClick={onIconClick}
+            className="absolute right-1 p-2 w-10 text-text-primary/60 hover:text-text-primary">
+            {icon}
+          </button>
+        )}
+      </div>
+      {errorText && <span className="text-error mt-1">{errorText}</span>}
     </div>
-    {errorText && <span className="text-error mt-1">{errorText}</span>}
-  </div>
-);
+  );
+};
