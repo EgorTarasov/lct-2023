0a7056f360d628fcd31a6f75b6a351b0ec450dd3
Kireev Kirill <karjaakirill@gmail.com>
2023-11-05 17:46:24 +0300

    add route architecture

---
+++ frontend/src/shared/components/SkipToContent.tsx
@@ -0,0 +1,7 @@
+export const SkipToContent = () => {
+  return (
+    <a className="block w-0 h-0 overflow-hidden focus:h-auto focus:w-auto" href={"#content"}>
+      К основной части
+    </a>
+  );
+};
--- frontend/src/shared/hoc/PrivateRoute.tsx
+++ frontend/src/shared/hoc/PrivateRoute.tsx
@@ -2,17 +2,20 @@ import { observer } from "mobx-react-lite";
 import { Navigate } from "react-router-dom";
 import React from "react";
 import { AuthService } from "@/stores/auth.service.ts";
-import { routesConfig } from "../../app";
 import { DesktopHeading, MobileNav } from "@/components/navigation";
+import { SkipToContent } from "@/components/SkipToContent";
+import { routes } from "../../app/routes";
 
 const PrivateRoute: React.FC<React.PropsWithChildren<unknown>> = observer(({ children }) => {
   const { status } = AuthService;
-  const currentRoute = routesConfig.find((route) => route.path === location.pathname);
+  const currentRoute = routes.find((route) => route.path === location.pathname);
   if (status === "loading") return null;
   if (status === "anonymous") return <Navigate to="/login" />;
   document.title = currentRoute?.title || "?";
   return (
     <>
+      <SkipToContent />
+
       <MobileNav />
       <DesktopHeading />
       <main id="content" tabIndex={-1} className={"flex-1"}>
@@ -22,4 +25,10 @@ const PrivateRoute: React.FC<React.PropsWithChildren<unknown>> = observer(({ chi
   );
 });
 
-export default PrivateRoute;
+export const withPrivateRoute = (Component: React.FC<unknown>) => {
+  return () => (
+    <PrivateRoute>
+      <Component />
+    </PrivateRoute>
+  );
+};
