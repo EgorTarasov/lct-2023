e50d2dca33ea92a322c8f3422257dd499a4de83c
Kireev Kirill <karjaakirill@gmail.com>
2023-11-05 23:50:19 +0300

    improve a-ty

--- frontend/src/pages/main/sections/assistant.section.tsx
+++ frontend/src/pages/main/sections/assistant.section.tsx
@@ -18,7 +18,11 @@ export const AssistantSection: FCVM<MainPageViewModel> = ({ vm }) => {
       <label htmlFor={assistantId} className="text-2xl font-bold text-white">
         Цифровой наставник
       </label>
-      <div className="flex bg-white rounded-xl p-3 items-center max-h-12">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="flex bg-white rounded-xl p-3 items-center max-h-12">
         <ChatBotIcon className="h-8 w-8" aria-hidden="true" focusable="false" />
         <input
           id={assistantId}
@@ -31,15 +35,15 @@ export const AssistantSection: FCVM<MainPageViewModel> = ({ vm }) => {
         <button className="w-fit" aria-label="Отправить вопрос">
           <SendIcon className="w-6 h-6" aria-hidden="true" focusable="false" />
         </button>
-      </div>
+      </form>
       <div className="flex flex-col gap-3">
         {mocData.map((item, index) => (
           <button
             key={index}
             onClick={() => setInputValue(item)}
-            className="px-3 py-1 border rounded-tr-xl rounded-br-xl rounded-bl-xl w-fit text-white bg-primary focus:bg-white focus:text-primary"
+            className="px-3 py-1 border rounded-tr-xl rounded-br-xl rounded-bl-xl w-fit text-white bg-primary focus-visible:bg-white focus-visible:text-primary"
             aria-label={`Спросить про ${item}`}>
-            <p className="text-base">{item}</p>
+            {item}
           </button>
         ))}
       </div>
