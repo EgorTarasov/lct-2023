966aef67944144bb7c9f00b4bb372ea9ae8fd4ef
nizhgo <alexey@nizhgorodov.ru>
2023-11-06 14:33:26 +0200

    hello EducationPage + Event page

---
+++ frontend/src/pages/education/education.page.tsx
@@ -0,0 +1,57 @@
+import { MockCourses } from "api/models/course.model.ts";
+import { CourseCard } from "../main/sections/course/course-card.widget.tsx";
+import { Input } from "@/ui/Input.tsx";
+import SearchIcon from "@/assets/search.svg";
+import { Chip } from "@/ui/chips.tsx";
+import { useState } from "react";
+
+enum Filter {
+  All,
+  Assigned,
+  NotAssigned,
+  Completed
+}
+
+export const EducationPage = () => {
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<Filter>(Filter.All);
+  return (
+    <div className="flex flex-col gap-4 px-4 py-6">
+      <Input
+        id={"search"}
+        placeholder={"Поиск"}
+        icon={<SearchIcon />}
+        className=""
+        value={search}
+        onChange={setSearch}
+      />
+      <div className={"flex items-center gap-2 flex-wrap"}>
+        <Chip
+          title={"Все"}
+          onClick={() => setFilter(Filter.All)}
+          isActive={filter === Filter.All}
+        />
+        <Chip
+          title={"Назначенные"}
+          onClick={() => setFilter(Filter.Assigned)}
+          isActive={filter === Filter.Assigned}
+        />
+        <Chip
+          title={"Не назначенные"}
+          onClick={() => setFilter(Filter.NotAssigned)}
+          isActive={filter === Filter.NotAssigned}
+        />
+        <Chip
+          title={"Завершенные"}
+          onClick={() => setFilter(Filter.Completed)}
+          isActive={filter === Filter.Completed}
+        />
+      </div>
+      <ul className="flex flex-col justify-between items-center gap-4">
+        {MockCourses.map((v, i) => (
+          <CourseCard item={v} key={v.id} wide />
+        ))}
+      </ul>
+    </div>
+  );
+};
--- frontend/src/pages/events/events.page.tsx
+++ frontend/src/pages/events/events.page.tsx
@@ -1,16 +1,68 @@
 import { EventCard } from "../main/sections/events/event-card.widget.tsx";
 import { MockEvents } from "api/models/event.model.ts";
+import { useState } from "react";
+import { Chip } from "@/ui/chips.tsx";
+import { Input } from "@/ui/Input.tsx";
+import SearchIcon from "@/assets/search.svg";
+
+enum Filter {
+  ForMe,
+  Sport,
+  Education,
+  Volunteering,
+  Creativity
+}
 
 export const EventsPage = () => {
+  const [search, setSearch] = useState("");
+  const [filters, setFilters] = useState<Filter[]>([Filter.ForMe]);
+
+  const handleFilter = (filter: Filter) => {
+    if (filters.includes(filter)) {
+      setFilters(filters.filter((f) => f !== filter));
+    } else {
+      setFilters([...filters, filter]);
+    }
+  };
+
   return (
-    <div>
-      <div
-        className={
-          "flex flex-col bg-yellow-400 w-full h-full items-center justify-center text-black"
-        }>
-        <code>⚒️ WORK IN PROGRESS ⚒️</code>
+    <div className="flex flex-col gap-4 px-4 py-6">
+      <Input
+        id={"search"}
+        placeholder={"Поиск"}
+        icon={<SearchIcon />}
+        className=""
+        value={search}
+        onChange={setSearch}
+      />
+      <div className={"flex items-center gap-2 flex-wrap"}>
+        <Chip
+          title={"Для меня"}
+          onClick={() => handleFilter(Filter.ForMe)}
+          isActive={filters.includes(Filter.ForMe)}
+        />
+        <Chip
+          title={"Спорт"}
+          onClick={() => handleFilter(Filter.Sport)}
+          isActive={filters.includes(Filter.Sport)}
+        />
+        <Chip
+          title={"Обучение"}
+          onClick={() => handleFilter(Filter.Education)}
+          isActive={filters.includes(Filter.Education)}
+        />
+        <Chip
+          title={"Волонтёрство"}
+          onClick={() => handleFilter(Filter.Volunteering)}
+          isActive={filters.includes(Filter.Volunteering)}
+        />
+        <Chip
+          title={"Творчество"}
+          onClick={() => handleFilter(Filter.Creativity)}
+          isActive={filters.includes(Filter.Creativity)}
+        />
       </div>
-      <ul className="flex-col justify-between items-center mx-4 mt-6 gap-4">
+      <ul className="flex flex-col justify-between items-center gap-4">
         {MockEvents.map((v, i) => (
           <EventCard item={v} onRegisterClick={() => console.log(v.id)} key={v.id} wide />
         ))}
