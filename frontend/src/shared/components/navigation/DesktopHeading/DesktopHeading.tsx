8e4069f995a0b461606b285361444d46ced86145
Kireev Kirill <karjaakirill@gmail.com>
2023-11-07 02:27:39 +0300

    fix layout

--- frontend/src/shared/components/navigation/DesktopHeading/DesktopHeading.tsx
+++ frontend/src/shared/components/navigation/DesktopHeading/DesktopHeading.tsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 
 export const DesktopHeading = () => {
   return (
-    <header className="hidden sm:flex">
-      <nav>
-        <ul>
-          <Link to="/main">Main</Link>
-        </ul>
-      </nav>
-      Heading
-    </header>
+    <>
+      {/* measurer */}
+      <div className="hidden sm:flex [grid-area:desktop] w-[340px]" aria-hidden="true" />
+      <header className="hidden sm:flex fixed left-0 bottom-0 top-0 w-[340px]">
+        <nav>
+          <ul>
+            <Link to="/main">Main</Link>
+          </ul>
+        </nav>
+        Heading
+      </header>
+    </>
   );
 };
