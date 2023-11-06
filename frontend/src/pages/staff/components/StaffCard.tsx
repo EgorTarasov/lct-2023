d78067d5b9c67fd56042f8b1a337ab4f3b763802
nizhgo <alexey@nizhgorodov.ru>
2023-11-06 19:38:37 +0200

    wip: staff page icons + a11y

--- frontend/src/pages/staff/components/StaffCard.tsx
+++ frontend/src/pages/staff/components/StaffCard.tsx
@@ -1,5 +1,8 @@
 import UserIcon from "@/assets/user.svg";
 import { StaffDto } from "api/models/staff.model.ts";
+import TelegramIcon from "@/assets/telegram-link.svg";
+import EmailIcon from "@/assets/email.svg";
+import PhoneIcon from "@/assets/phone.svg";
 
 export const StaffCard: React.FC<StaffDto.User> = ({
   id,
@@ -13,11 +16,11 @@ export const StaffCard: React.FC<StaffDto.User> = ({
   return (
     <article
       className="flex flex-col gap-4 p-4 bg-white rounded-lg border border-text-primary/20"
-      aria-labelledby="staffName">
+      aria-labelledby="Карточка сотрудника">
       <div className="flex gap-2 items-start">
         <UserIcon className="w-11 h-11 text-primary" aria-hidden="true" />
         <div className="flex-col gap-1">
-          <div className="flex gap-2 items-center" aria-label="Roles">
+          <div className="flex gap-2 items-center" aria-label="Роли сотрудника">
             {roles.map((role, index) => (
               <>
                 {index !== 0 && <div className="w-1 h-1 bg-primary rounded-full not-sr-only" />}
@@ -27,26 +30,31 @@ export const StaffCard: React.FC<StaffDto.User> = ({
               </>
             ))}
           </div>
-          <p id="staffName">{fullName}</p>
+          <p id="staffName" aria-label={"ФИО сотрудника"}>
+            {fullName}
+          </p>
         </div>
       </div>
-      <div className="flex flex-col gap-0.5 text-sm">
+      <div className="flex flex-col gap-0.5 text-sm" aria-label={`Контакты ${fullName}`}>
         <a
-          aria-label={`Телефон ${fullName}`}
+          aria-label={"Телефон"}
           href={formatPhoneHref}
           className={"break-all underline text-primary"}>
+          <PhoneIcon className="w-4 h-4 inline-block mr-1" aria-hidden="true" />
           {phone}
         </a>
         <a
           href={`mailto:${email}`}
-          aria-label={`Почта ${fullName}`}
+          aria-label={"Почта"}
           className="break-all underline text-primary">
+          <EmailIcon className="w-4 h-4 inline-block mr-1" aria-hidden="true" />
           {email}
         </a>
         <a
-          aria-label={`Телеграм ${fullName}`}
-          href={`https://t.me/${telegram}`}
+          aria-label={"Телеграм"}
+          href={`${telegram.replace("@", "https://t.me/")}`}
           className={"break-all underline text-primary"}>
+          <TelegramIcon className="w-4 h-4 inline-block mr-1" aria-hidden="true" />
           {telegram}
         </a>
       </div>
