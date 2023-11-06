b25546198bb513e5e36cd00340e086db2f455857
nizhgo <alexey@nizhgorodov.ru>
2023-11-06 22:41:18 +0200

    mobile hello

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,14 +1,13 @@
 import "./index.css";
-import { Link, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import { ThemeService } from "@/stores/theme.service.ts";
 import { observer } from "mobx-react-lite";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 import "./transitions.scss";
-import { RoutesWithoutNav, routes } from "./routes";
-import { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { routes, RoutesWithoutNav } from "./routes";
+import { useLayoutEffect, useState } from "react";
 import { SkipToContent } from "@/components/SkipToContent";
-import { DesktopHeading, MobileNav } from "@/components/navigation";
-import { Footer } from "@/components/footer";
+import { DesktopLayout, MobileNav } from "@/components/navigation";
 
 const App = observer(() => {
   const location = useLocation();
@@ -30,26 +29,26 @@ const App = observer(() => {
     return <div className={"w-full h-full flex items-center justify-center"}>⏳Загрузка...</div>;
 
   return (
-    <>
+    <div className={"relative z-0 flex h-full w-full overflow-hidden flex-col"}>
       {!RoutesWithoutNav.includes(location.pathname) && (
         <>
           <MobileNav />
-          <DesktopHeading />
         </>
       )}
       <SkipToContent />
-      <main id="content" tabIndex={-1} className={"flex-1 h-max-content"}>
-        <SwitchTransition>
-          <CSSTransition key={location.key} classNames="fade" timeout={150} unmountOnExit>
-            <Routes location={location}>
-              {MappedRoutes}
-              <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-          </CSSTransition>
-        </SwitchTransition>
-      </main>
-      <Footer />
-    </>
+      <DesktopLayout>
+        <main id="content" tabIndex={-1} className={"relative z-0 h-full w-full overflow-hidden"}>
+          <SwitchTransition>
+            <CSSTransition key={location.key} classNames="fade" timeout={150} unmountOnExit>
+              <Routes location={location}>
+                {MappedRoutes}
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </CSSTransition>
+          </SwitchTransition>
+        </main>
+      </DesktopLayout>
+    </div>
   );
 });
 
--- frontend/src/shared/components/navigation/DesktopHeading/DesktopHeading.tsx
+++ frontend/src/shared/components/navigation/DesktopHeading/DesktopHeading.tsx
@@ -1,14 +1,66 @@
-import { Link } from "react-router-dom";
+import { routes } from "../../../../app/routes.tsx";
+import { NavLink } from "react-router-dom";
+import ChevronIcon from "@/assets/chevron2.svg";
+import { ThemeService } from "@/stores/theme.service.ts";
+import LightningIcon from "@/assets/lightning.svg";
+import UserIcon from "@/assets/user.svg";
 
-export const DesktopHeading = () => {
+export const DesktopLayout = ({ children }: React.PropsWithChildren<unknown>) => {
+  const currentRoute = routes.find((route) => route.path === location.pathname);
   return (
-    <header className="hidden sm:flex">
-      <nav>
-        <ul>
-          <Link to="/main">Main</Link>
-        </ul>
-      </nav>
-      Heading
+    <div className="relative z-0 flex h-full w-full overflow-hidden bg-[#EDEDED]">
+      <div
+        className={"dark flex-shrink-0 overflow-x-hidden bg-white border border-text-primary/20"}>
+        <div className={"h-full w-[260px]"}>
+          <img src={ThemeService.themeConfig?.logoUrl} alt="Логотип" className="h-12" />
+          {routes
+            .filter((route) => route.showInNav)
+            .map((item) => (
+              <li
+                key={item.path}
+                className={
+                  item.path === currentRoute?.path ? "text-primary" : "text-text-primary/80"
+                }>
+                <NavLink
+                  to={item.path}
+                  className="flex items-center justify-between border-b border-nav-text py-3">
+                  {item.title}
+                  {item.path !== currentRoute?.path && <ChevronIcon className="w-6 h-6" />}
+                </NavLink>
+              </li>
+            ))}
+        </div>
+      </div>
+      <div className="relative flex h-full max-w-full flex-1 flex-col overflow-hidden">
+        <DesktopHeading />
+        <main className="relative h-full w-full overflow-auto">
+          <div className={"max-w-[1024px] mx-auto flex-1 w-full"}>
+            <div className={"flex h-full flex-col"}>
+              <div className={"flex-1 h-full"}>
+                <div className={"h-full w-full"}>{children}</div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+const DesktopHeading = () => {
+  return (
+    <header className="sticky top-4 left-4 right-4 z-10">
+      <div className="flex items-center justify-end gap-4 max-w-[1024px] mx-auto rounded-full bg-white w-full px-4 py-2">
+        <NavLink to={"/shop"} aria-label="Перейти в магазин">
+          <div className="flex items-center gap-2" aria-label={"Ваша баланс: 100"}>
+            <LightningIcon className="w-6 h-6 text-primary" />
+            <span className="text-base text-text-primary">100</span>
+          </div>
+        </NavLink>
+        <NavLink to={"/me"} aria-label="Перейти в профиль">
+          <UserIcon className="w-10 h-10 text-primary" />
+        </NavLink>
+      </div>
     </header>
   );
 };
