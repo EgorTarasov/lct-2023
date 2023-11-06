18013a3f1aae6c4d16c85cd36a2f28d5109ce510
Kireev Kirill <karjaakirill@gmail.com>
2023-11-07 01:32:40 +0300

    improvements

--- frontend/src/pages/tasks/tasks.vm.ts
+++ frontend/src/pages/tasks/tasks.vm.ts
@@ -1,6 +1,9 @@
+import { MockTasks, TaskDto } from "api/models/task.model";
 import { makeAutoObservable } from "mobx";
 
 export class TasksPageViewModel {
+  public tasks: TaskDto.Item[] | null = MockTasks;
+
   constructor() {
     makeAutoObservable(this);
   }
