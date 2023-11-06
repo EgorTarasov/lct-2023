d24386289efe043a2bbd9848f6ee8ea697ca45e8
Kireev Kirill <karjaakirill@gmail.com>
2023-11-06 21:07:50 +0300

    fix layout

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,8 +4,11 @@ import { ThemeService } from "@/stores/theme.service.ts";
 import { observer } from "mobx-react-lite";
 import { CSSTransition, SwitchTransition } from "react-transition-group";
 import "./transitions.scss";
-import { routes } from "./routes";
+import { RoutesWithoutNav, routes } from "./routes";
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { SkipToContent } from "@/components/SkipToContent";
+import { DesktopHeading, MobileNav } from "@/components/navigation";
+import { Footer } from "@/components/footer";
 
 const App = observer(() => {
   const location = useLocation();
@@ -19,15 +22,7 @@ const App = observer(() => {
 
   const [MappedRoutes] = useState(() =>
     routes.map((route, index) => (
-      <Route
-        key={index}
-        path={route.path}
-        element={
-          <div>
-            <route.component />
-          </div>
-        }
-      />
+      <Route key={index} path={route.path} element={<route.component />} />
     ))
   );
 
@@ -35,14 +30,26 @@ const App = observer(() => {
     return <div className={"w-full h-full flex items-center justify-center"}>⏳Загрузка...</div>;
 
   return (
-    <SwitchTransition>
-      <CSSTransition key={location.key} classNames="fade" timeout={150} unmountOnExit>
-        <Routes location={location}>
-          {MappedRoutes}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </CSSTransition>
-    </SwitchTransition>
+    <>
+      {!RoutesWithoutNav.includes(location.pathname) && (
+        <>
+          <MobileNav />
+          <DesktopHeading />
+        </>
+      )}
+      <SkipToContent />
+      <main id="content" tabIndex={-1} className={"flex-1 h-max-content"}>
+        <SwitchTransition>
+          <CSSTransition key={location.key} classNames="fade" timeout={150} unmountOnExit>
+            <Routes location={location}>
+              {MappedRoutes}
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </CSSTransition>
+        </SwitchTransition>
+      </main>
+      <Footer />
+    </>
   );
 });
 
