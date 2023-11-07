c7744207f10d1ceed7e837ab71046e6e409df55b
Kireev Kirill <karjaakirill@gmail.com>
2023-11-07 20:43:33 +0300

    adaptive design

--- frontend/src/pages/main/main.page.tsx
+++ frontend/src/pages/main/main.page.tsx
@@ -7,24 +7,34 @@ import { CourseSection } from "./sections/course/course.section";
 import { observer } from "mobx-react-lite";
 import { DiveinSection } from "./sections/divein.section";
 
-const card = "bg-white rounded-2xl sm:p-4";
+const card = "bg-white w-full rounded-2xl sm:p-4";
 
 export const MainPage = observer(() => {
   const [vm] = useState(() => new MainPageViewModel());
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4">
       <AssistantSection vm={vm} />
-      <div className="grid sm:grid-cols-2 sm:mx-4 sm:gap-4">
-        <div className={card}>
-          <OnBoardingSection vm={vm} />
+      <div className="flex flex-col gap-4 w-full mx-auto max-w-screen-desktop">
+        <div
+          className="grid sm:mx-4 gap-4"
+          style={{
+            gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))"
+          }}>
+          <div className={card}>
+            <OnBoardingSection vm={vm} />
+          </div>
+          <div className={card}>
+            <CourseSection vm={vm} />
+          </div>
         </div>
-        <div className={card}>
-          <CourseSection vm={vm} />
+        <div className={`sm:w-auto sm:mx-4 ${card}`}>
+          <DiveinSection vm={vm} />
+        </div>
+        <div className={`sm:w-auto sm:mx-4 ${card}`}>
+          <EventsSection vm={vm} />
         </div>
       </div>
-      <DiveinSection vm={vm} />
-      <EventsSection vm={vm} />
     </div>
   );
 });
