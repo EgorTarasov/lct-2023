5d5f5041a54ed56bba722e5ac309037115c2478b
Андрей <kondrandr2004@yandex.ru>
2023-11-09 22:32:18 +0300

    Notifications

--- backend/app/crud/task.py
+++ backend/app/crud/task.py
@@ -1,6 +1,24 @@
 from sqlalchemy.orm import Session
 
-from app.models.task import TaskCreate, SqlTask, TaskDto
+from app.models.task import TaskCreate, SqlTask
+
+
+async def get_task_by_id(db: Session, task_id: int) -> SqlTask:
+    """Получение задачи пользователя"""
+    task = db.query(SqlTask).where(SqlTask.id == task_id).one()
+    return task
+
+
+async def get_tasks(db: Session, user_id: int) -> list[SqlTask]:
+    """Получение задач пользователя"""
+    tasks = db.query(SqlTask).where(SqlTask.mentee_id == user_id).all()
+    return tasks
+
+
+async def get_tasks_for_mentor(db: Session, mentor_id: int) -> list[SqlTask]:
+    """Получение задач подопечных ментора"""
+    tasks = db.query(SqlTask).filter(SqlTask.mentor_id == mentor_id).all()
+    return tasks
 
 
 async def create_task(db: Session, payload: TaskCreate, mentor_id) -> SqlTask:
@@ -13,26 +31,6 @@ async def create_task(db: Session, payload: TaskCreate, mentor_id) -> SqlTask:
     return db_task
 
 
-async def get_tasks(db: Session, user_id: int) -> list[TaskDto]:
-    """Получение задач пользователя"""
-    tasks = db.query(SqlTask).where(SqlTask.mentee_id == user_id).all()
-    return tasks
-
-
-async def get_tasks_for_mentor(db: Session, mentor_id: int) -> list[TaskDto]:
-    """Получение задач подопечных ментора"""
-    tasks = db.query(SqlTask).filter(SqlTask.mentor_id == mentor_id).all()
-    return tasks
-
-
-async def delete_task(db: Session, task_id: int):
-    """Удаление задачи по id"""
-    deleted_rows = db.query(SqlTask).where(SqlTask.id == task_id).delete()
-    if deleted_rows == 0:
-        raise Exception("Такой задачи не существует")
-    db.commit()
-
-
 async def change_task(db: Session, task_id: int, payload: TaskCreate) -> SqlTask:
     """Изменение задачи по id"""
     db_task = db.query(SqlTask).where(SqlTask.id == task_id).one_or_none()
@@ -43,3 +41,11 @@ async def change_task(db: Session, task_id: int, payload: TaskCreate) -> SqlTask
     db.commit()
     db.refresh(db_task)
     return db_task
+
+
+async def delete_task(db: Session, task_id: int):
+    """Удаление задачи по id"""
+    deleted_rows = db.query(SqlTask).where(SqlTask.id == task_id).delete()
+    if deleted_rows == 0:
+        raise Exception("Такой задачи не существует")
+    db.commit()
