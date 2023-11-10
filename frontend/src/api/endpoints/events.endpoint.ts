c10f5f0e92407800830997785be2fec0058dc3ba
Kireev Kirill <karjaakirill@gmail.com>
2023-11-11 00:05:52 +0300

    finish main page

--- frontend/src/api/endpoints/events.endpoint.ts
+++ frontend/src/api/endpoints/events.endpoint.ts
@@ -1,7 +1,7 @@
 import { EventDto } from "api/models/event.model";
 import api from "api/utils/api";
 
-export namespace EventEndpoint {
+export namespace EventsEndpoint {
   export const current = async () => {
     return await api.get<EventDto.Result[]>("/api/event/my");
   };
--- frontend/src/pages/main/main.vm.ts
+++ frontend/src/pages/main/main.vm.ts
@@ -1,4 +1,5 @@
 import { CourseEndpoint } from "api/endpoints/course.endpoint";
+import { EventsEndpoint } from "api/endpoints/events.endpoint";
 import { TasksEndpoint } from "api/endpoints/tasks.endpoint";
 import { CourseDto, MockCourses } from "api/models/course.model";
 import { EventDto, MockEvents } from "api/models/event.model";
@@ -6,7 +7,7 @@ import { MockTasks, TaskDto } from "api/models/task.model";
 import { makeAutoObservable } from "mobx";
 
 export class MainPageViewModel {
-  public events: EventDto.Item[] | null = MockEvents;
+  public events: EventDto.Item[] | null = null;
   public courses: CourseDto.Item[] | null = null;
   public tasks: TaskDto.Item[] | null = null;
 
@@ -16,11 +17,13 @@ export class MainPageViewModel {
   }
 
   private async init() {
-    const [coursesRes, tasksRes] = await Promise.all([
+    const [coursesRes, tasksRes, eventsRes] = await Promise.all([
       CourseEndpoint.current(),
-      TasksEndpoint.current()
+      TasksEndpoint.current(),
+      EventsEndpoint.current()
     ]);
     this.courses = coursesRes.map(CourseDto.convertDtoToItem);
     this.tasks = tasksRes.map(TaskDto.convertDtoToItem);
+    this.events = eventsRes.map(EventDto.convertDtoToItem);
   }
 }
