987393cd2eddb9cb3522d27e7a6ebad59e437e48
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-11-10 19:15:55 +0300

    добавление архива к курсу

--- backend/app/controllers/file_controller.py
+++ backend/app/controllers/file_controller.py
@@ -1,7 +1,10 @@
 import aiofiles
 import uuid
 import pathlib
+import os
+import shutil
 import typing as tp
+import zipfile
 
 from fastapi import File
 from sqlalchemy.orm import Session
@@ -14,6 +17,7 @@ class FileController:
         self.db = db
 
     async def save_file(self, file: bytes, filename: str) -> SqlFile:
+
         _, _type = filename.split(".")
         file_path = "./static/"
         static_name = str(uuid.uuid4()) + _type
@@ -23,6 +27,32 @@ class FileController:
 
         db_file = crud.file.save(self.db, filename=filename, path=file_path)
         return db_file
+
+    async def save_files(self, _zip: bytes, zip_name: str) -> list[SqlFile]:
+        tmp_path = "./static/tmp"
+        zip_name = zip_name.split(".")[0]
+        with open("./static/test.zip", "wb") as f:
+            f.write(_zip)
+
+        with zipfile.ZipFile("./static/test.zip", "r") as zip_ref:
+            zip_ref.extractall(tmp_path)
+            zip_files = [
+                tmp_path + "/" + name for name in os.listdir(tmp_path + "/" + zip_name)
+            ]
+
+            print(f"files: {zip_files}")
+
+        files = {}
+        for file in zip_files:
+            static_name = f"./static/{str(uuid.uuid4())}"
+            file_name = file.split("/")[-1]
+            file_path = f"{tmp_path}/{zip_name}/{file_name}"
+            shutil.move(file_path, static_name)
+
+            files[file_name] = static_name
+
+        db_files = crud.file.save_all(self.db, files)
+        return db_files
 
     async def get_file(self, file_id: int) -> SqlFile:
         db_file = crud.file.get(self.db, file_id)
--- backend/app/crud/file.py
+++ backend/app/crud/file.py
@@ -10,6 +10,16 @@ def save(db: Session, filename: str, path: str) -> SqlFile:
     return db_file
 
 
+def save_all(db: Session, files: dict[str, str]) -> list[SqlFile]:
+    print(files)
+    db_files = [SqlFile(name=k, path=v) for k, v in files.items()]
+    db.add_all(db_files)
+    db.commit()
+    for f in db_files:
+        db.refresh(f)
+    return db_files
+
+
 def get(db: Session, id: int) -> SqlFile:
     result = db.query(SqlFile).filter(SqlFile.id == id).first()
     if not result:
