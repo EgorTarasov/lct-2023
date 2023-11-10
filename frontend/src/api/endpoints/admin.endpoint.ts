33ace8286e8292e5839e5df51fa8e11070e35233
Kireev Kirill <karjaakirill@gmail.com>
2023-11-10 21:39:11 +0300

    add mentees

--- frontend/src/api/endpoints/admin.endpoint.ts
+++ frontend/src/api/endpoints/admin.endpoint.ts
@@ -5,4 +5,8 @@ export namespace AdminEndpoint {
   export const registerUser = async (user: UserDto.Update) => {
     return await api.post<string>("/api/admin/register", user);
   };
+
+  export const getMentees = async () => {
+    return await api.get<UserDto.Item[]>("/api/mentor/me");
+  };
 }
