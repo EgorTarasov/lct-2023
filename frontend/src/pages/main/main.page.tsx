4360bc677475e7030ab98bc9c6f2c525203106e9
Kireev Kirill <karjaakirill@gmail.com>
2023-11-11 00:05:52 +0300

    finish main page

--- frontend/src/pages/main/main.page.tsx
+++ frontend/src/pages/main/main.page.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { AssistantSection } from "./sections/assistant.section";
 import { MainPageViewModel } from "./main.vm";
-import { OnBoardingSection } from "./sections/onboarding.section";
 import { EventsSection } from "./sections/events/events.section";
 import { CourseSection } from "./sections/course/course.section";
 import { observer } from "mobx-react-lite";
