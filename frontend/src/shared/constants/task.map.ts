db79f8069a107b62f7e77114cc0456287edf8d41
Aleksey Nizhgorodov <nizhgorodov.ao@perfema.com>
2023-11-11 02:56:36 +0200

    hot fix

--- frontend/src/shared/constants/task.map.ts
+++ frontend/src/shared/constants/task.map.ts
@@ -1,34 +1,36 @@
 import { TaskDto } from "api/models/task.model";
 
-const TaskCategoryMap: Readonly<
-  Record<
-    TaskDto.TaskType,
-    {
-      locale: string;
-      textColor: string;
-    }
-  >
-> = {
-  education: {
-    textColor: "text-task-education",
-    locale: "Обучение"
-  },
-  divein: {
-    textColor: "text-task-divein",
-    locale: "Погружение"
-  },
-  event: {
-    textColor: "text-task-event",
-    locale: "Мероприятие"
-  },
-  meeting: {
-    textColor: "text-task-meeting",
-    locale: "Встреча"
-  },
-  work: {
-    textColor: "text-task-work",
-    locale: "Работа"
+export const getTaskMap = (type: TaskDto.TaskType): { textColor: string; locale: string } => {
+  switch (type) {
+    case "Обучение":
+      return {
+        textColor: "text-task-education",
+        locale: "Обучение"
+      };
+    case "Адаптация":
+      return {
+        textColor: "text-task-divein",
+        locale: "Погружение"
+      };
+    case "Мероприятие":
+      return {
+        textColor: "text-task-event",
+        locale: "Мероприятие"
+      };
+    case "Собрание":
+      return {
+        textColor: "text-task-meeting",
+        locale: "Встреча"
+      };
+    case "Работа":
+      return {
+        textColor: "text-task-work",
+        locale: "Работа"
+      };
+    default:
+      return {
+        textColor: "text-task-education",
+        locale: "Обучение"
+      };
   }
 };
-
-export const getTaskMap = (type: TaskDto.TaskType) => TaskCategoryMap[type];
