8a324166253fc13aec41940cfb9c466b1d6b0202
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-11-11 05:23:05 +0300

    telegram test 3

--- backend/app/models/telegram.py
+++ backend/app/models/telegram.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, Text, ForeignKey
+
+from .base import Base
 
 
 class TelegramLoginData(BaseModel):
@@ -9,3 +13,15 @@ class TelegramLoginData(BaseModel):
     photo_url: str | None = None
     auth_date: int
     hash: str
+
+
+class SqlTelegram(Base):
+    __tablename__ = "telegram"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    first_name: Mapped[str] = mapped_column(Text)
+    last_name: Mapped[str] = mapped_column(Text, nullable=True)
+    username: Mapped[str] = mapped_column(Text, nullable=True)
+    photo_url: Mapped[str] = mapped_column(Text, nullable=True)
+    auth_date: Mapped[int] = mapped_column(Integer)
