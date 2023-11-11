4fc95080f6a8d65da81d240c77b0a2d604e22ee3
Kireev Kirill <karjaakirill@gmail.com>
2023-11-11 22:42:09 +0300

    fix

--- frontend/src/pages/admin/analytics/analytics.vm.ts
+++ frontend/src/pages/admin/analytics/analytics.vm.ts
@@ -1,5 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import { AnalyticsEndpoint } from "api/endpoints/analytics.endpoint.ts";
+import { getStoredAuthToken } from "api/utils/authToken";
 
 export class AnalyticsViewModel {
   isAllDataLoading = false;
@@ -12,15 +13,22 @@ export class AnalyticsViewModel {
   getAllData = async () => {
     this.isAllDataLoading = true;
     await runInAction(async () => {
-      const byteCharacters: ArrayBuffer = await AnalyticsEndpoint.getActions();
-      //download file
-      const url = window.URL.createObjectURL(new Blob([byteCharacters]));
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/analytics/actions`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${getStoredAuthToken()}`,
+          "Access-Control-Allow-Origin": "*"
+        },
+        method: "GET"
+      });
+      const url = window.URL.createObjectURL(new Blob([await res.blob()]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "actions.xlsx");
+      link.setAttribute("download", "statistics.xlsx");
       document.body.appendChild(link);
       link.click();
     });
+    this.isAllDataLoading = false;
   };
 
   getOnboardingData = async () => {
